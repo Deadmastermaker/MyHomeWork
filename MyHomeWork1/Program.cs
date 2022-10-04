2632953d22006e40e5aae70e99633c2ad0a93dcb
unknown <artem.antonovski@gmail.com>
2022-10-04 18:31:57 +0300

    Homework Artyom

--- MyHomeWork1/Program.cs
+++ MyHomeWork1/Program.cs
@@ -40,5 +40,35 @@ if(c > max)
     */
 
 
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
 
+/*Console.Write("Input number: ");
+int digit =  Convert.ToInt32(Console.ReadLine());
+
+if(digit % 2 == 0 )
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+while(current<=num)
+{
+    if(num%2==0)
+    {
+    Console.Write(" " + num);
+    }
+}
+num--;
+Console.Write("Input N");
+
+*/
